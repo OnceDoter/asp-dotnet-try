7dc9e10372891efb139a064e42266fc9d8a3e520
OnceDoter <gordeev_p@mail.ru>
2020-10-18 21:04:54 +0700

    now work

--- WebApi.Tests/ContentRepoTests.fs
+++ WebApi.Tests/ContentRepoTests.fs
@@ -1,5 +1,6 @@
 module WebApi.Tests
 
+open Microsoft.AspNetCore.Mvc
 open NUnit.Framework
 open System.Threading.Tasks
 open WebApi.Controllers.Pictures
@@ -7,7 +8,6 @@ open WebApi.Data
 open Microsoft.EntityFrameworkCore
 open Microsoft.Extensions.Configuration
 open WebApi.Data.Models
-open System.Collections
 
 let threadsCount = 10
 let testEntitiesCount = 50
@@ -27,42 +27,45 @@ let contexts = [
 [<SetUp>]
 let Setup () = ()
 
-let isEmpty (x:obj) =
-    match x with
-    | null -> true
-    | :? System.Collections.IEnumerable as xs -> xs |> Seq.cast |> Seq.isEmpty
-    | _ -> invalidOp <| "unsupported type " + x.GetType().FullName
-
 [<Test>]
 let PictureService_change_entities_in_10_threads_return_no_DbUpdateConcurrencyException () =
-    let expected = ()
-    //use best practics
-    let fail (methName : string) = Assert.Fail <| "<"+methName+">" + "return null"
-    //init concurrent servises
+    let expected = true
+    //init concurrent services
     //create test entities
-    let iservices =  [for i in 0..threadsCount -> new PictureService (contexts.[0])]
-    iservices.Head.Create([|for i in 0..testEntitiesCount do new Image("test descr #" + i.ToString())|]) |> ignore
-    let imgs = iservices.[0].GetImages() |> Seq.cast<Image>
-    if isEmpty imgs then fail "GetImages()"
+    let services =  [for i in 0..threadsCount-1 -> new PictureService (contexts.[i])]
+    
+    services.Head.Create(
+                            [|for i in 0..testEntitiesCount-1 do new Image("test descr #" + i.ToString())|]
+                        ) |> ignore
+    let images = services.[1].GetImages()
+    let enum = images.GetEnumerator()
+    let tasks = Array.zeroCreate threadsCount
+    //methods need to check result
+    let check (result : ActionResult) : bool =
+        match result with
+        | ok when ok = (upcast new OkResult() : ActionResult) -> true
+        | _ -> false
+    let mutable list = [true]
+    let add (result : bool) : unit =
+        list <- result :: list
     //simulate concurrent access
+    try
     let act (service : PictureService) = 
-        for i in 0..testEntitiesCount do 
-            ()
-    let actual = for service in iservices do Task.Run(act(service))
-                    
-
-
-    let ares = Array.zeroCreate testEntitiesCount
-    let vres = Array.zeroCreate testEntitiesCount
-    (*let actual = async {
-        let imgs = repos.Images.GetImages() |> Seq.cast<Image>
-        imgs.[1]
-        for e in 0 .. testEntitiesCount do
-            Task.Factory.StartNew(
-                fun() -> for img in imgs do
-                    
-            )
-        
-    }*)
-    let actual = ()
+        async{
+            for i in 0..testEntitiesCount-1 do service.Update(enum.Current)
+                                               |> check
+                                               |> add
+        } |> ignore
+    for i in 0..threadsCount-1 do
+            tasks.[i] =
+                Task.Factory.StartNew(
+                                         fun () -> act <| services.[i]
+                                     ) |> ignore
+    finally
+    Task.WaitAll()
+    //lets check
+    let mutable actual = true
+    for item in list do
+            if (not item) then actual <- false    
+            
     Assert.AreEqual(expected, actual)
